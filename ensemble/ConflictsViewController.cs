29771a3f0e050d08a5d4460e0ce45484333ab0f3
gdc24 <gdc24@case.edu>
2020-02-24 00:59:36 -0500

    add a bunch of pages

---
+++ ensemble/ConflictsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace ensemble
+{
+    public partial class ConflictsViewController : UITableViewController
+    {
+        public ConflictsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ ensemble/ScheduleDetailViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace ensemble
+{
+    public partial class ScheduleDetailViewController : UIViewController
+    {
+        public ScheduleDetailViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}

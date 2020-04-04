e07dcd74f626d9ff0043adc14757a714469248f7
Ryan Arvizu <raa116@case.edu>
2020-04-04 13:22:20 -0400

    Create PayloadsTableViewController.designer.cs

---
+++ ensemble/PayloadsTableViewController.designer.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ensemble {
+    
+    [Register ("PayloadsTableViewController")]
+    
+    partial class PayloadsTableViewController {
+        void ReleaseDesignerOutlets () {}
+    }
+    
+}

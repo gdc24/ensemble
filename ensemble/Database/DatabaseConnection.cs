5d536aab1b2fdcb13149d7b2a0669187dee9fc67
gdc24 <gdc24@case.edu>
2020-04-20 16:16:18 -0400

    start database connection file

---
+++ ensemble/Database/DatabaseConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Npgsql;
+
+namespace ensemble
+{
+    public class DatabaseConnection
+    {
+        public static readonly string SERVER = "";
+        public static readonly string USER = "";
+        public static readonly string DATABASE = "";
+        public static readonly string PASS = "";
+
+        public static NpgsqlConnection GetConnection()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("" +
+                "Server=" + SERVER + ";" +
+                "User Id=" + USER + ";" +
+                "Password=" + PASS + ";" +
+                "Database=" + DATABASE + ";");
+
+            return conn;
+        }
+
+    }
+}

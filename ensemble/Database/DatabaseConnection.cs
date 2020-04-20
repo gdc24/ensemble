5a47ba26aa2149c8ce1ad4efcb4eb4d0d9f4c54a
gdc24 <gdc24@case.edu>
2020-04-20 16:18:41 -0400

    add databsae connection credentials

--- ensemble/Database/DatabaseConnection.cs
+++ ensemble/Database/DatabaseConnection.cs
@@ -5,10 +5,10 @@ namespace ensemble
 {
     public class DatabaseConnection
     {
-        public static readonly string SERVER = "";
-        public static readonly string USER = "";
-        public static readonly string DATABASE = "";
-        public static readonly string PASS = "";
+        public static readonly string SERVER = "ec2-52-6-143-153.compute-1.amazonaws.com";
+        public static readonly string USER = "gyevrymixnlzst";
+        public static readonly string DATABASE = "dceh4hrbfiso1o";
+        public static readonly string PASS = "e1930311b68ec1fdc7c8e1efca93161c3503ecd271bd07b47d85d3fbcc052a32";
 
         public static NpgsqlConnection GetConnection()
         {
